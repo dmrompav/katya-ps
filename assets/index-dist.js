686684b0ae80b91a769eff2e21d6cea3f370692d
dmrompav <dm.rom.pav@gmail.com>
2021-05-16 15:19:34 +0500

    saving again

---
+++ assets/index-dist.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var horButtons = document.querySelectorAll('.hor-sli__item');
+var verSliders = document.querySelectorAll('.ver-sli');
+var verButtons = [];
+
+for (var i = 0; i < verSliders.length; i++) {
+  verButtons[i] = verSliders[i].querySelectorAll('.ver-sli__item');
+}
+
+var ModalContent = [[{
+  name: 'Обо мне',
+  p: 'Я ученица 10А класса МБОУ "ПМШ №44". Я увлекаюсь информатикой и программированием. В будущем я хочу стать frontend-разработчиком.'
+}, {
+  name: 'О проекте',
+  p: 'Данная страница является частью проектной работы на тему "Разработка frontend части web-приложения<br/><br/>Этот сайт является сайтом-резюме, который поможет потенциальному работадателю узнать о моих навыках и умениях, а также связаться со мной.'
+}], [{
+  name: 'Английский',
+  p: 'На данный момент мой уровень знания английского языка Advanced(C1)'
+}, {
+  name: 'Немецкий',
+  p: 'На данный момент мой уровень знания немецкого языка Begginer(A1)'
+}, {
+  name: 'HTML',
+  p: 'Прошла курс из 50 блоков в прилоении SoloLearn'
+}, {
+  name: 'CSS',
+  p: 'Прошла курс из 35 блоков в приложении SoloLearn'
+}, {
+  name: 'JS',
+  p: 'Прошло курс из 47 блоков в приложении SoloLearn'
+}], [], [{
+  name: 'Мама',
+  p: 'Моя дочь очень хорошая программистка! Она умеет включать компьютер и налаживать принтер! Вау! Я в шоке!'
+}], []];
+var x = 0;
+var y = [0, 0, 0, 0, 0];
+horButtons.forEach(function (btn, ind) {
+  btn.addEventListener('click', function () {
+    horButtons[x].classList.remove('hor-sli__item--selected');
+    horButtons[x].classList.add('hor-sli__item--selectable');
+    document.querySelector('.hor-sli').classList.remove("hor-sli--".concat(x));
+    document.querySelector('.all-ver').classList.remove("all-ver--".concat(x));
+    document.querySelectorAll('.all-ver__ver-sli')[x].classList.add('all-ver__ver-sli--selectable');
+    document.querySelectorAll('.all-ver__ver-sli')[x].classList.remove('all-ver__ver-sli--selected');
+    x = ind;
+    horButtons[x].classList.add('hor-sli__item--selected');
+    horButtons[x].classList.remove('hor-sli__item--selectable');
+    document.querySelector('.hor-sli').classList.add("hor-sli--".concat(x));
+    document.querySelector('.all-ver').classList.add("all-ver--".concat(x));
+    document.querySelectorAll('.all-ver__ver-sli')[x].classList.add('all-ver__ver-sli--selected');
+    document.querySelectorAll('.all-ver__ver-sli')[x].classList.remove('all-ver__ver-sli--selectable');
+  });
+});
+verButtons.forEach(function (slider) {
+  slider.forEach(function (btn, ind) {
+    btn.addEventListener('click', function () {
+      verButtons[x][y[x]].classList.add('ver-sli__item--selectable');
+      verButtons[x][y[x]].classList.remove('ver-sli__item--selected');
+      verSliders[x].classList.remove("ver-sli--".concat(y[x]));
+      y[x] = ind;
+      verButtons[x][y[x]].classList.add('ver-sli__item--selected');
+      verButtons[x][y[x]].classList.remove('ver-sli__item--selectable');
+      verSliders[x].classList.add("ver-sli--".concat(y[x]));
+
+      if (btn.classList.contains('openable')) {
+        CallModal();
+      }
+    });
+  });
+});
+
+function CallModal() {
+  document.querySelector('.modal').classList.add('modal--opened');
+  document.querySelector('.modal__name').innerHTML = ModalContent[x][y[x]].name;
+  document.querySelector('.modal__content').innerHTML = ModalContent[x][y[x]].p;
+  document.querySelector('.modal__tapfield').addEventListener('click', CloseModal, false);
+}
+
+function CloseModal() {
+  document.querySelector('.modal').classList.remove('modal--opened');
+}
